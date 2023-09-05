a910e87cf16dcdcf92fd63118cfd14863572cd33
Ewin24 <edwintrigos24@gmail.com>
2023-09-05 02:12:21 -0700

    feat: inicio de desarrollo, seccion de productos

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { cargarProductos } from "./productos/productos.js";
+// import { eliminar } from "./consultaAPI.js";
+
+
+cargarProductos();
+
+let btnEliminar = document.querySelectorAll('.btn-danger');
